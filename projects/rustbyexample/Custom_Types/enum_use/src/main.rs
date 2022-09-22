7dd62bfba9de6369400e956eed5db5bad2fa595e
estherjanesamuel <esther.jane.samuel@gmail.com>
2022-09-22 17:37:14 +0700

    add enum use

---
+++ projects/rustbyexample/Custom_Types/enum_use/src/main.rs
@@ -0,0 +1,47 @@
+/*
+    The use declaration can be used so manual scoping isn't needed
+*/
+
+// An attribut to hide warnings for unused code.
+#![allow(dead_code)]
+
+#[derive(Debug)]
+enum Status {
+    Rich,
+    Poor,
+}
+
+
+#[derive(Debug)]
+enum Work {
+    Civilian,
+    Soldier,
+}
+
+fn main() {
+
+    // Explicitly 'use' each name so ther are available without manual scoping
+    use crate::Status::{Poor, Rich};
+    // automatically 'use' each name inside 'Work
+    use crate::Work::*;
+
+    // Equivalent to 'Status::Poor
+    let status = Poor;
+
+    // Equivalent to Work::Civilian
+    let work = Civilian;
+
+    match status {
+        // note the lack of scoping because the explicit 'use' above.
+        Rich => println!("The rich have lots money!"),
+        Poor => println!("The poor have no money..."),
+    }
+
+    match work {
+        // note again the lack of scoping.
+        Civilian => println!("Civilians work!"),
+        Soldier => println!("Soldiers fight!"),
+    }
+
+    println!("Status : {:?} , Work : {:?}", status, work);
+}
