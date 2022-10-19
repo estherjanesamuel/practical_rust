21ccb8b2d162f7264c12d3f5f4e0351e8b4e3f3e
estherjanesamuel <esther.jane.samuel@gmail.com>
2022-10-19 16:29:34 +0700

    error syntax

--- projects/rust_sqlite/rust_sqlite_api/src/main.rs
+++ projects/rust_sqlite/rust_sqlite_api/src/main.rs
@@ -1,3 +1,10 @@
+use actix_web::http::StatusCode;
+use actix_web::web::{Data, Json};
+use actix_web::{web,HttpResponse,Error};
+use anyhow::{Result, Ok};
+
+pub mod routes;
+
 fn main() {
     println!("Hello, world!");
 }
